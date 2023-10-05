480c652eeeecdd4990f4d0fa50d1e4d296f2ba69
Heder Barreto <matheusmr640@gmail.com>
2023-10-04 23:47:17 -0300

    add index

---
+++ index.php
@@ -0,0 +1,12 @@
+
+<!DOCTYPE html>
+<html lang="en">
+
+<?php include "views/head.php"; ?>
+
+<body>
+    <?php include "views/menu.php"; ?>
+    <h1 align="center">FUNCIONARIO MANAGER!</h1>
+</body>
+
+</html>
