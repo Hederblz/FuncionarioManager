62d06e7c14b83ac05d301a7577da703e60a70bfc
Heder Barreto <matheusmr640@gmail.com>
2023-10-04 23:45:36 -0300

    add models

---
+++ Models/CargoModel.php
@@ -0,0 +1,29 @@
+<?php
+
+
+class CargoModel{
+
+    private $id;
+    private $descricao;
+
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function setDescricao($descricao)
+    {
+        $this->descricao = $descricao;
+    }
+
+    public function getDescricao()
+    {
+        return $this->descricao;
+    }
+
+}
---
+++ Models/FuncionarioModel.php
@@ -0,0 +1,63 @@
+<?php
+
+
+require_once "CargoModel.php";
+
+class FuncionarioModel{
+
+    private $id;
+    private $nome;
+    private $sobrenome;
+    private $salario;
+    private $codCargo;
+
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function setNome($nome)
+    {
+        $this->nome = $nome;
+    }
+
+    public function getNome()
+    {
+        return $this->nome;
+    }
+
+    public function setSobrenome($sobrenome)
+    {
+        $this->sobrenome = $sobrenome;
+    }
+
+    public function getSobrenome()
+    {
+        return $this->sobrenome;
+    }
+
+    public function setSalario($salario)
+    {
+        $this->salario = $salario;
+    }
+
+    public function getSalario()
+    {
+        return $this->salario;
+    }
+
+    public function setCodCargo($codCargo)
+    {
+        $this->codCargo = $codCargo;
+    }
+
+    public function getCodCargo()
+    {
+        return $this->codCargo;
+    }
+}
